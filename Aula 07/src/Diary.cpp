4c6271e6704344f9c1f74302baece11c1eefb830
Gravenai <roberland.ricardo@gmail.com>
2020-06-23 17:05:51 -0300

    Compiler adapter

---
+++ Aula 07/src/Diary.cpp
@@ -0,0 +1,16 @@
+#include "../include/Diary.h"
+
+Diary::Diary(const std::string& filename)
+{
+	//this->filename = filename; 
+}
+
+void Diary::add(const std::string& message){
+    // adicionar mensagem no array de mensagens
+    //messages[message_size + 1] = message;
+}
+
+void Diary::write()
+{
+    // gravar as mensagens no disco
+}
